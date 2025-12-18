443fdb85e0ae2bfd8e628d3a0afd1a1177abaa73
bala-amypo <bala@amypo.in>
2025-12-18 09:02:10 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/Skill.java
@@ -0,0 +1,8 @@
+package com.example.deemo.entity;
+
+public class Skill{
+    private long id;
+    private String name;
+    private String category;
+    private 
+}
--- src/main/java/com/example/demo/model/UserProfile.java
+++ src/main/java/com/example/demo/model/UserProfile.java
@@ -1,5 +1,6 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.*;
 import java.sql.Timestamp;
 
 public class UserProfile {
